24e03fd1ab809bfd66b10e706c07e1047a84c76f
sapars <unstd@ya.ru>
2022-08-01 17:10:15 +0300

    Создание основы программы

---
+++ ChartExamples.py
@@ -0,0 +1,12 @@
+#Примеры построения графиков
+
+import tkinter as tk
+
+#Создание главного окна
+window = tk.Tk()
+window.geometry("450x450")
+window.title("Программа анализа .csv файлов")
+
+#Запуск окна программы
+window.mainloop()
+ 
