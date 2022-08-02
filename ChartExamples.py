dc253644f60443dd4307bcacfd580fa375584834
sapars <unstd@ya.ru>
2022-08-02 09:48:06 +0300

    Добавление вывода графика синуса

--- ChartExamples.py
+++ ChartExamples.py
@@ -2,6 +2,9 @@
 
 import tkinter as tk
 
+#Импорт внешних файлов
+import chart1
+
 #Функция закрытия прграммы
 def do_close():
     window.destroy()
@@ -17,12 +20,18 @@ lbTitle = tk.Label(text="Примеры построения графиков", 
 lbTitle.place(x=55, y=25)
 
 #Добавление кнопки и метки для графика 1
-btnChart1 = tk.Button(window, text="График 1", font=('Helvetica', 10, 'bold'), fg='#0000cc')
+btnChart1 = tk.Button(window, text="График 1", font=('Helvetica', 10, 'bold'), command=chart1.plot_chart)
 btnChart1.place(x=40, y=115, width=90, height=30)
 
 lbChart1 = tk.Label(text="График синуса matplotlib")
 lbChart1.place(x=170, y=122)
 
+#Добавление кнопки и метки для графика 2
+btnChart1 = tk.Button(window, text="График 2", font=('Helvetica', 10, 'bold'), command=chart1.plot_chart)
+btnChart1.place(x=40, y=165, width=90, height=30)
+
+lbChart1 = tk.Label(text="Нормальное распределение")
+lbChart1.place(x=170, y=172)
 
 #Добавление кнопки закрытия программы
 btnClose = tk.Button(window, text="Закрыть", font=('Helvetica', 10, 'bold'), command=do_close)
