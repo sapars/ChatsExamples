ee31483cc1ad2e6810fe0b6bd95b5eb95bb043f3
sapars <unstd@ya.ru>
2022-08-02 09:37:03 +0300

    Добавление кнопки для графика1

--- ChartExamples.py
+++ ChartExamples.py
@@ -12,9 +12,16 @@ window = tk.Tk()
 window.geometry("450x450")
 window.title("Программа построения графиков")
 
-#Добавлени метки заголовка
+#Добавление метки заголовка
 lbTitle = tk.Label(text="Примеры построения графиков", font=('Helvetica', 16, 'bold'), fg='#0000cc')
 lbTitle.place(x=55, y=25)
+
+#Добавление кнопки и метки для графика 1
+btnChart1 = tk.Button(window, text="График 1", font=('Helvetica', 10, 'bold'), fg='#0000cc')
+btnChart1.place(x=40, y=115, width=90, height=30)
+
+lbChart1 = tk.Label(text="График синуса matplotlib")
+lbChart1.place(x=170, y=122)
 
 
 #Добавление кнопки закрытия программы
